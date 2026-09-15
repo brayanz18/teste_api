8df6805e212d67353378eabf20ff80aa90cbf899
brayanz18 <gabrielbrayan994@gmail.com>
2026-09-15 14:22:05 -0300

    primeiro commit

---
+++ src/main/java/teste_api/brayan_gabriel/BrayanGabrielApplication.java
@@ -0,0 +1,13 @@
+package teste_api.brayan_gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrayanGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrayanGabrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste_api/brayan_gabriel/BrayanGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package teste_api.brayan_gabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrayanGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
